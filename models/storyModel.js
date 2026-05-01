c377df1dedffcd74c00f0cd9f6c2623695a8306e
Samudini Gunarathna <samudinigunarathna@gmail.com>
2026-05-02 01:03:37 +0530

    Update storySchema.js

--- models/storyModel.js
+++ models/storyModel.js
@@ -1,1 +1,24 @@
 import mongoose from "mongoose";
+
+const storySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+
+    author: {
+        type: String,
+        required: true,
+    },
+
+    category: {
+        type: String,
+        required: true,
+    },
+
+    content: {
+        type: String,
+    },
+});
+
+export default mongoose.Schema("stories", storySchema);
