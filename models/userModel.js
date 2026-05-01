a0f884ded0ea6db0a229777d6c99090ced00915a
Samudini Gunarathna <samudinigunarathna@gmail.com>
2026-05-02 00:54:18 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,2 +1,25 @@
 import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+
+    email:{
+        type: String,
+        required: true,
+    },
+
+    password:{
+        type: String,
+        required: true,
+    },
+
+    childName: {
+        type: String,
+        required: true,
+    }
+});
+
+export default mongoose.model("users", userSchema);
