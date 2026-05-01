cca73dd0c26b4cd998c4a5751d6ff353c93e0e15
Samudini Gunarathna <samudinigunarathna@gmail.com>
2026-05-02 01:11:53 +0530

    Update favourites.js

--- models/favouriteModel.js
+++ models/favouriteModel.js
@@ -1,1 +1,17 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose";import mongoose from "mongoose";
+
+const favouriteSchema = new mongoose.Schema({
+    userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true,
+    },
+
+    storyID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "stories",
+        required: true,
+    },
+})
+
+export default mongoose.Schema("favourites", favouriteSchema);
