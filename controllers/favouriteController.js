d32aca1cd8a1d5f545cf3c0b889a69f75fd6982d
Samudini Gunarathna <samudinigunarathna@gmail.com>
2026-05-05 18:10:00 +0530

    update favouriteController.js

--- controllers/favouriteController.js
+++ controllers/favouriteController.js
@@ -23,3 +23,43 @@ export const create = async(req, res)=>{
     }
 }
 
+//For getting all  from the database
+export const fetch = async(req, res) => {
+    try{
+        //Find all favourites in the database
+        const stories = await story.find({userID, storyID});
+        //Check if stories are not available, then send a error response
+        const favouriteExist = await favourite.find({userID: req.params.userID});
+        if(!favouriteExist || favouriteExist.length === 0){
+            return res.status(404).json({message: "No favourite stories found for this user."})
+        }
+        //Send a success response
+        res.status(200).json(stories);
+    }
+
+    catch(error){
+        //Handle any errors
+        res.status(500).json({message: "Internal server error."})
+    }
+}
+
+//For deleting data from the database
+export const deleteFavourite = async(req, res) => {
+    try{
+        //Extract story id from request parameters
+        const id = req.params.id;
+        const favouriteExist = await story.findOne({_id:id});
+        if(!favouriteExist){
+            return res.status(404).json({Message: "Story not exists"})
+        }
+        //Delete the story from the database
+        await story.findByIdAndDelete(id);
+        //Send a success response
+        res.status(201).json({Message: "Story deleted succesfully."}); 
+    }
+
+    catch(error){
+        //Handle any errors
+        res.status(500).json({message: "Internal server error."})
+    }
+}
